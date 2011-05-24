4fa9d698828a0c367edd8e529e5e00c1b2e668ec
Kentaro Kuribayashi <kentarok@gmail.com>
2011-05-24 16:57:11 +0900

    initial import

---
+++ lib/Net/Hatena.pm
@@ -0,0 +1,39 @@
+package Net::Hatena;
+use strict;
+use warnings;
+use 5.008001;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Net::Hatena -
+
+=head1 SYNOPSIS
+
+  use Net::Hatena;
+
+=head1 DESCRIPTION
+
+Net::Hatena is
+
+=head1 AUTHOR
+
+Kentaro Kuribayashi E<lt>kentarok@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Kentaro Kuribayashi
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
